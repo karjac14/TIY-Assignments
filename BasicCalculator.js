a959926f220ca7dfe9ca4374883c73fc7cabf186
Karlo Eleuterio <karloe@gmail.com>
2015-09-01 17:15:03 -0400

    Added Four Math Functions

--- BasicCalculator.js
+++ BasicCalculator.js
@@ -1,11 +1,32 @@
 console.assert(add(0, 0), /* FILL ME IN */);
 console.assert(add(0, 1), /* FILL ME IN */);
 console.assert(add(0, 2), /* FILL ME IN */);
-
+console.assert(minus(0, 3), /* FILL ME IN */);
+console.assert(minus(6, 4), /* FILL ME IN */);
+console.assert(minus(7, 5), /* FILL ME IN */);
+console.assert(minus(3, 6), /* FILL ME IN */);
+console.assert(times(0, 7), /* FILL ME IN */);
+console.assert(times(0, 8), /* FILL ME IN */);
+console.assert(divide(0, 9), /* FILL ME IN */);
+console.assert(divide(1, 1), /* FILL ME IN */);
+console.assert(divide(2, 5), /* FILL ME IN */);
 /**
  * @param {Number} A to add to B
  * @param {Number} B to add to A
  * @return {Number} sum of A and B
  */
-function add(A, B){
+function add(A,B){
+  return A + B;
 } // END add
+
+function minus(A,B) {
+  return A-B;
+}
+
+function times (A,B) {
+  return A*B;
+}
+
+function divide (A,B) {
+  return A/B;
+}
