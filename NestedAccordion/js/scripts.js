6e08ec548a49c5aa852f413edecc8e6e937d1bb7
Karlo Eleuterio <karloe@gmail.com>
2015-09-09 23:03:50 -0400

    Updated Nested Accordion and View Modes Switch

---
+++ NestedAccordion/js/scripts.js
@@ -0,0 +1,15 @@
+The webpage shows a sample of navigation buttons. Each buttons
+has short text decription under each one. Another three buttons are nested
+which also have some text under it. The DOM which in this case, is consisted of
+an HTML, CSS and Javascript. The HTML is the backbone of the DOM, where in
+different CSS and JS files are being "called" or linked.
+
+The movements from the text when clicking the buttons are interactive. Looking
+at the dev tools, the <li> is having class of "cbp-nttrigger".
+
+Clicking the buttons activates the movement, and clicking again resets back the
+the original potion of the texts under the button.
+
+Linking a JS file that will control the bahavious of the buttons is needed.
+Perhaps a JS script that accepts an input of click or hover will do. The JS
+will also have an output changing the class of objects in the html.index
---
+++ ViewModeSwitch/js/scripts.js
@@ -0,0 +1,12 @@
+The webpage shows a template for an online selling website. The website is
+responsive. The website also have minimalist blue design.
+
+There is a button or "switch" that changes the outline how the items are show.
+The two modes are grid and list. The grid shows the large photos of the items
+while the list shows smaller photos but the user can see more items at the
+same time.
+
+The javscrpt is ecpected to invloved in the behaviour of the switches. It can
+take an input of click from the modes of the view. The output will give a
+different container size (where the items are show). Another JS files also
+the classes of the html object to change the behaviours.
