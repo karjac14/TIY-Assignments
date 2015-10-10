1336782e8577d00fea0713a204940ce0de12b517
Karlo Eleuterio <karloe@gmail.com>
2015-10-10 12:31:13 -0400

    update main.js and responsive.scss

---
+++ HorizontalSlideOutMenu-HT/src/js/main.js
@@ -0,0 +1,21 @@
+//tggle for panels
+
+$('a[href="#nav-one"]').on('click', function(){
+  $('#nav-one').toggleClass('active');
+});
+
+$('a[href="#nav-two"]').on('click', function(){
+  $('#nav-two').toggleClass('active');
+});
+
+$('a[href="#nav-three"]').on('click', function(){
+  $('#nav-three').toggleClass('active');
+});
+
+$('a[href="#nav-four"]').on('click', function(){
+  $('#nav-four').toggleClass('active');
+});
+
+$('a[href="#nav-five"]').on('click', function(){
+  $('#nav-five').toggleClass('active');
+});
