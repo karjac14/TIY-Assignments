1db36cdbb3790841afd62f72b2f55c4a22e2a5e9
Karlo Eleuterio <karloe@gmail.com>
2015-10-11 10:38:40 -0400

    tweaks and refactor on tabs

--- HorizontalSlideOutMenu-HT/src/js/main.js
+++ HorizontalSlideOutMenu-HT/src/js/main.js
@@ -3,24 +3,34 @@
 $('a#nav-one').on('click', function(){
   $(this).toggleClass('active');
   $('#panel-one').slideToggle('50');
+  $('#panel-two,#panel-three,#panel-four,#panel-five').hide(); // to refactor?
+  $('a[id*="nav-"]').not(this).removeClass('active'); //refactored
 });
 
 $('a#nav-two').on('click', function(){
   $(this).toggleClass('active');
   $('#panel-two').slideToggle('50');
+  $('#panel-one,#panel-three,#panel-four,#panel-five').hide(); // to refactor?
+  $('a[id*="nav-"]').not(this).removeClass('active'); //refactored
 });
 
 $('a#nav-three').on('click', function(){
   $(this).toggleClass('active');
   $('#panel-three').slideToggle('50');
+  $('#panel-one,#panel-two,#panel-four,#panel-five').hide(); // to refactor?
+  $('a[id*="nav-"]').not(this).removeClass('active'); //refactored
 });
 
 $('a#nav-four').on('click', function(){
   $(this).toggleClass('active');
   $('#panel-four').slideToggle('50');
+  $('#panel-one,#panel-two,#panel-three,#panel-five').hide(); // to refactor?
+  $('a[id*="nav-"]').not(this).removeClass('active'); //refactored
 });
 
 $('a#nav-five').on('click', function(){
   $(this).toggleClass('active');
   $('#panel-five').slideToggle('50');
+  $('#panel-one,#panel-two,#panel-three,#panel-four').hide(); // to refactor?
+  $('a[id*="nav-"]').not(this).removeClass('active'); //refactored
 });
